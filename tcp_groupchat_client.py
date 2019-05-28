1c465d63da254e53ef5800cfa543fa597b39a4f0
listen2099 <listen2099@gmail.com>
2019-05-28 14:46:05 +0800

    ok

---
+++ tcp_groupchat_client.py
@@ -0,0 +1,48 @@
+import socket
+import threading
+import logging
+
+FORMAT = "%(asctime)s %(threadName)s %(thread)d %(message)s"
+logging.basicConfig(format=FORMAT, level=logging.INFO)
+
+
+class ChatClient:
+    def __init__(self, rip='127.0.0.1', rport=9999):
+        self.raddr = (rip, rport)
+        self.sock = socket.socket()
+        self.event = threading.Event()
+
+    def start(self):
+        self.sock.connect(self.raddr)
+
+        threading.Thread(target=self.recv, name='recv').start()
+
+    def recv(self):
+        while not self.event.is_set():
+            data = self.sock.recv(1024)
+            logging.info(data)
+
+    def send(self, msg):
+        self.sock.send(msg.encode())
+
+    def stop(self):
+        self.sock.close()
+        self.event.set()
+
+
+def main():
+
+    cc = ChatClient()
+    cc.start()
+
+    while True:
+        cmd = input('>>>')
+        if cmd.strip() == 'quit':
+            cc.stop()
+            break
+        cc.send(cmd)
+        logging.info(threading.enumerate())
+
+
+if __name__ == '__main__':
+    main()

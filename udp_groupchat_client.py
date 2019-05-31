cd00145caf525c884a2da33dc243ce87f73547ac
listen2099 <listen2099@gmail.com>
2019-05-31 14:32:05 +0800

    ok

--- udp_groupchat_client.py
+++ udp_groupchat_client.py
@@ -7,20 +7,25 @@ logging.basicConfig(format=FORMAT, level=logging.INFO)
 
 
 class ChatUdpClient:
-    def __init__(self, rip='127.0.0.1', rport=9999):
+    def __init__(self, rip='127.0.0.1', rport=9999, interval=10):
         self.raddr = (rip, rport)
         self.sock = socket.socket(type=socket.SOCK_DGRAM)
         self.event = threading.Event()
+        self.interval = interval
 
     def start(self):
         self.sock.connect(self.raddr)
+        threading.Thread(target=self.heartbeat, name='heartbeat').start()
         threading.Thread(target=self.recv, name='recv').start()
 
     def recv(self):
         while not self.event.is_set():
-            data, addr = self.sock.recvfrom(1024)
-            # addr ?= raddr
+            data, addr = self.sock.recvfrom(1024)  # addr ?= raddr
             logging.info(data)
+
+    def heartbeat(self):
+        while not self.event.wait(self.interval):
+
 
     def send(self, msg: str = 'quit'):
         self.sock.sendto(msg.encode(), self.raddr)
@@ -31,7 +36,7 @@ class ChatUdpClient:
 
 
 def main():
-    cc = ChatUdpClient()
+    cc = ChatUdpClient(interval=5)
     cc.start()
 
     while True:

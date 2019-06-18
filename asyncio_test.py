5d35481bf0a305da8f75172889e48f78201cc9be
listen2099 <listen2099@gmail.com>
2019-06-18 11:31:32 +0800

    ok

--- asyncio_test.py
+++ asyncio_test.py
@@ -1,29 +1,81 @@
 import threading
+import asyncio
 
 
-def a(x=3):
+# @asyncio.coroutine
+# def a(x=3):
+#     for i in range(x):
+#         out = 'a.x {}'.format(i)
+#         print(out)
+#         # threading.Event().wait(0.001)
+#         yield
+#
+#
+# @asyncio.coroutine
+# def b(x=3):
+#     for i in range(x):
+#         out = 'b.x {}'.format(i)
+#         print(out)
+#         # threading.Event().wait(0.001)
+#         yield
+
+async def a(x=3):
     for i in range(x):
         out = 'a.x {}'.format(i)
-        # print(out)
+        print(out)
         # threading.Event().wait(0.001)
-        yield out
+        await asyncio.sleep(0.0001)
 
 
-def b(x=3):
+async def b(x=3):
     for i in range(x):
         out = 'b.x {}'.format(i)
-        # print(out)
+        print(out)
         # threading.Event().wait(0.001)
-        yield out
+        await asyncio.sleep(0.0001)
 
 
 # threading.Thread(target=a).start()
 # threading.Thread(target=b).start()
 
 
-a = a()
-b = b()
+# a = a()
+# b = b()
+#
+# for i in range(3):  # 必须有循环
+#     print(next(a))
+#     print(next(b))
 
-for i in range(3):  # 必须有循环
-    print(next(a))
-    print(next(b))
+loop = asyncio.get_event_loop()
+tasks = [a(), b()]
+loop.run_until_complete(asyncio.wait(tasks))
+loop.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
